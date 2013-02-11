aa87dc4ba0fa27f9585f1af99e547c99cbdedd34
z4k <stefano.cudini@gmail.com>
2013-02-11 06:10:31 +0100

    added files, ad simplest demo

---
+++ README.md
@@ -0,0 +1,33 @@
+Leaflet.Control.GPS
+============
+
+What ?
+------
+
+A leaflet control that active gps locate
+
+Tested in Leaflet 0.4.4
+
+How ?
+------
+
+Adding the search control to the map:
+
+```
+
+map.addControl(new L.Control.Gps();
+
+and insert leaflet-gps.css styles to your css page
+
+```
+
+Where ?
+------
+
+Source code:
+	https://github.com/stefanocudini/leaflet-search
+	https://bitbucket.org/zakis_/leaflet-search
+
+Demos:
+	http://easyblog.it/maps/leaflet-gps/
+
---
+++ leaflet-gps.js
@@ -0,0 +1,101 @@
+/*
+ * Leaflet Search Control 1.0.0
+ * https://github.com/stefanocudini/leaflet-gps
+ * https://bitbucket.org/zakis_/leaflet-gps
+ * http://easyblog.it/maps/leaflet-gps
+ *
+ * Copyright 2012, Stefano Cudini - stefano.cudini@gmail.com
+ * Licensed under the MIT license.
+ */
+
+L.Control.Gps = L.Control.extend({
+	includes: L.Mixin.Events, 
+	
+    options: {
+        position: 'topleft',
+        gpsLayer: null,
+        //TODO autoActive: false,
+        title: 'Center map on your location'
+    },
+
+	initialize: function(options) {
+		L.Util.setOptions(this, options);
+		this.options.gpsLayer = this.options.gpsLayer || new L.LayerGroup();
+		this._stateGps = false;//global state of gps
+	},
+	
+    onAdd: function (map) {
+    
+    	this._map = map;
+		this._circleGps = (new L.CircleMarker([0,0], {radius: 20, weight:3, color: '#e03', fill: false})).addTo(this._map);
+        	
+        var container = L.DomUtil.create('div', 'leaflet-control-gps'),
+            button = L.DomUtil.create('a', 'gps-button', container);
+        button.href = '#';
+        button.title = this.options.title;
+
+        L.DomEvent
+			.disableClickPropagation(button)
+			.addListener(button, 'click', this._activeGps, this);
+			//TODO use this._stateGps for switch _activeGps/_deactivGps 
+			
+		L.DomEvent
+			.addListener(map, 'locationfound', this._drawGps, this);
+		//TODO refact animation on locationfound, look under
+			
+//		L.DomEvent
+//			.addListener(map, 'locationerror', function(){ this._activeGps = false; }, this);//TODO refact
+
+        return container;
+    },
+    
+	onRemove: function(map) {
+		this._deactiveGps();
+		//TODO destroy this._circleGps
+	},
+    
+    _activeGps: function() {
+	    this._map.locate({
+	        setView: true,
+	        enableHighAccuracy: true
+			//watch:true
+			//maximumAge:s	        
+	    });
+    },
+    
+    _deactiveGps: function() {
+		this._map.stopLocate();
+		this._stateGps = false;
+    },
+    
+    _drawGps: function(e) {
+    	//e.accuracy	//TODO use for circle radius/color
+    	//e.bounds
+    	this._stateGps = true;
+    	this._circleGps.setLatLng(e.latlng);
+    },
+
+//TODO refact animation on locationfound
+//	_animateLocation: function(latlng) {
+//	
+//		var circle = this._circleGps;
+//		circle.setLatLng(latlng);
+//		circle.setRadius(20);
+//	
+//		var	tt = 200,
+//			ss = 10,
+//			mr = parseInt(circle._radius/ss),
+//			f = 0;
+//		var	that = this;
+//		this.timerAnimLoc = setInterval(function() {  //animation
+//			f += 0.5;
+//			mr += f;//adding acceleration
+//			var nr = circle._radius - mr;
+//			if( nr > 2)
+//				circle.setRadius(nr);
+//			else
+//				clearInterval(that.timerAnimLoc);
+//		}, tt);
+//	},
+
+});
