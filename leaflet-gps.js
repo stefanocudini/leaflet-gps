413bb556ec340e2541a7875b1846597c729288bb
z4k <stefano.cudini@gmail.com>
2013-02-25 00:27:22 +0100

    bugifx style option

--- leaflet-gps.js
+++ leaflet-gps.js
@@ -26,7 +26,8 @@ L.Control.Gps = L.Control.extend({
 	},
 
 	initialize: function(options) {
-		options.style = L.Util.extend({}, this.options.style, options.style); 
+		if(options.style)
+			options.style = L.Util.extend({}, this.options.style, options.style); 
 		L.Util.setOptions(this, options);
 		this._stateGps = false;//global state of gps
 	},
